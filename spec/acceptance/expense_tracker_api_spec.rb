66b122c88a586c1a44c08d4d1dfdc58d213583c7
José Luis Varela <varela@fintual.com>
2023-11-28 22:59:47 -0300

    Add get request to get expenses by date

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -11,13 +11,45 @@ end
 RSpec.describe "Expense Tracker Api" do
   include Rack::Test::Methods
 
+  def post_expense(expense)
+    post "/expenses", JSON.generate(expense)
+
+    expect(last_response.status).to eq 200
+
+    parsed = JSON.parse(last_response.body)
+    expect(parsed).to include("expense_id" => a_kind_of(Integer))
+    expense.merge("id" => parsed["expense_id"])
+  end
+
   it "record submitted expenses" do
+    pending "Need to persist expenses"
+
     coffee = {
       "payee" => "Starbucks",
       "amount" => 5.75,
       "date" => "2017-06-10"
     }
 
-    post "/expenses", JSON.generate(coffee)
+    zoo = {
+      "payee" => "Zoo",
+      "amount" => 15.25,
+      "date" => "2017-06-10"
+    }
+
+    groceries = {
+      "payee" => "Whole Foods",
+      "amount" => 95.20,
+      "date" => "2017-06-11"
+    }
+
+    post_expense(coffee)
+    post_expense(zoo)
+    post_expense(groceries)
+
+    get "/expenses/2017-06-10"
+    expect(last_response.status).to eq 200
+
+    expenses = JSON.parse(last_response.body)
+    expect(expenses).to contain_exactly(coffee, zoo)
   end
 end
